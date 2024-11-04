b76c30b6af56362aaec625f92f07ef2628906303
flabodi <flavius123.n@gmail.com>
2024-11-04 15:54:17 +0100

    S2-G1

---
+++ D1.js
@@ -0,0 +1,79 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+let tipo_stringa= " questa é una variabile modificabile_di testo"
+tipo_stringa = "questa_modificata "
+console.log("tipo_stringa")
+const nonModificabile = "questa rimarra fissa "
+console.log(nonModificabile)
+let questaPerValoreNumerico = "33"
+console.log(questaPerValoreNumerico)
+let questoBooleanVeroFalso = "true"
+console.log(questoBooleanVeroFalso)
+
+let questaStringaE_vuota;
+console.log(questaStringaE_vuota)
+let comeSopraMaIntenzionalmenteVuota = null; 
+console.log(comeSopraMaIntenzionalmenteVuota)
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+const myName = "flavius"
+console.log( "flavius" + myName ) 
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+const numero1= 12
+const numero2= 20
+const somma =numero1+numero2 
+console.log("la somma è " + somma )
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+const x = 12
+console.log(x)
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+myName = "bodescu"
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+const y = 4 
+const sottrazione = y-x 
+console.log("la sottraziobe è" + sottrazione)
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+const name1 = "john"
+const name2 = "John"
+const verificaUguaglianza = name1==name2 
+console.log(" uguaglianza " + verificaUguaglianza )
+/* SCRIVI QUI LA TUA RISPOSTA */
